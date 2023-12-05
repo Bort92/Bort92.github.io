4ecc56e3f434b59c4f4b294727b6e829d70c8a4c
Courtney Tregurtha-Nairn <132959509+Bort92@users.noreply.github.com>
2023-12-06 09:47:09 +1300

    Update KKT_Comm_WebApp.js

--- KKT_Comm_WebApp.js
+++ KKT_Comm_WebApp.js
@@ -1,103 +1,103 @@
 require([
-  "esri/WebMap",
-  "esri/views/MapView",
-  "esri/layers/FeatureLayer",
-  "esri/widgets/LayerList",
-  "esri/widgets/Attachments",
-  "esri/identity/IdentityManager"
-], function(WebMap, MapView, FeatureLayer, LayerList, Attachments, IdentityManager) {
-
-  IdentityManager.registerOAuthInfos([{
-    appId: "rLMgQSOnVhjpZUI2",  // replace with your app id
-    portalUrl: "https://horizonsrc.maps.arcgis.com"  // replace with your organization's URL
-  }]);
-
-  var webmap = new WebMap({
-      portalItem: {
-        id: "341248e420144a8a9fe2f4ac4065d199",
-        apiKey:"AAPK7e43c4ab59e2403ab56144750045a6abA7OjBlbqZA2lDrLq0uFLvI9NmC1zK1UkctlwBUmUT8WOEgKKxUbkTDfv8oQ02uFJ"
-      }
-  });
-
-  var view = new MapView({
-    container: "viewDiv",
-    map: webmap
-  });
-
-  const clusterConfig = {
-    type: "cluster",
-    clusterRadius: "100px",
-    popupTemplate: {
-      title: "Cluster summary",
-      content: "This cluster represents {cluster_count} projects.",
-      fieldInfos: [{
-        fieldName: "cluster_count",
-        format: {
-          places: 0,
-          digitSeparator: true
+    "esri/WebMap",
+    "esri/views/MapView",
+    "esri/layers/FeatureLayer",
+    "esri/widgets/LayerList",
+    "esri/widgets/Attachments",
+    "esri/identity/IdentityManager"
+  ], function(WebMap, MapView, FeatureLayer, LayerList, Attachments, IdentityManager) {
+  
+    IdentityManager.registerOAuthInfos([{
+      appId: "rLMgQSOnVhjpZUI2",  // replace with your app id
+      portalUrl: "https://horizonsrc.maps.arcgis.com"  // replace with your organization's URL
+    }]);
+  
+    var webmap = new WebMap({
+        portalItem: {
+          id: "341248e420144a8a9fe2f4ac4065d199",
+          apiKey:"AAPK7e43c4ab59e2403ab56144750045a6abA7OjBlbqZA2lDrLq0uFLvI9NmC1zK1UkctlwBUmUT8WOEgKKxUbkTDfv8oQ02uFJ"
         }
+    });
+  
+    var view = new MapView({
+      container: "viewDiv",
+      map: webmap
+    });
+  
+    const clusterConfig = {
+      type: "cluster",
+      clusterRadius: "100px",
+      popupTemplate: {
+        title: "Cluster summary",
+        content: "This cluster represents {cluster_count} projects.",
+        fieldInfos: [{
+          fieldName: "cluster_count",
+          format: {
+            places: 0,
+            digitSeparator: true
+          }
+        }]
+      },
+      clusterMinSize: "24px",
+      clusterMaxSize: "60px",
+      labelingInfo: [{
+        deconflictionStrategy: "none",
+        labelExpressionInfo: {
+          expression: "Text($feature.cluster_count, '#,###')"
+        },
+        symbol: {
+          type: "text",
+          color: "#004a5d",
+          font: {
+            weight: "bold",
+            family: "Noto Sans",
+            size: "12px"
+          }
+        },
+        labelPlacement: "center-center",
       }]
-    },
-    clusterMinSize: "24px",
-    clusterMaxSize: "60px",
-    labelingInfo: [{
-      deconflictionStrategy: "none",
-      labelExpressionInfo: {
-        expression: "Text($feature.cluster_count, '#,###')"
-      },
-      symbol: {
-        type: "text",
-        color: "#004a5d",
-        font: {
-          weight: "bold",
-          family: "Noto Sans",
-          size: "12px"
-        }
-      },
-      labelPlacement: "center-center",
-    }]
-  };
-
-  var layer = new FeatureLayer({
-    url: "https://services1.arcgis.com/VuN78wcRdq1Oj69W/arcgis/rest/services/KKT_Community_Projects/FeatureServer",
-    featureReduction: clusterConfig,
-    outFields: ["*"],
-    popupTemplate: {
-      title: "{District}",
-      content: "This point represents a project in the {District} district."
-    }
-  });
-
-  webmap.add(layer);
-
-  var layerList = new LayerList({
-    view: view
-  });
-
-  view.ui.add(layerList, "top-right");
-
-  var attachments;
-
-  view.when(function() {
-    attachments = new Attachments({
-      view: view,
-      layer: layer
-    });
-
-    view.ui.add(attachments, "bottom-right");
-  });
-
-  view.on("click", function(event) {
-    view.hitTest(event).then(function(response) {
-      var result = response.results.filter(function(result) {
-        return result.graphic.layer === layer;
-      })[0];
-      if (result) {
-        layer.selectFeatures([result.graphic]);
-
-        // Set the feature for the Attachments widget
-        attachments.feature = result.graphic;
+    };
+  
+    var layer = new FeatureLayer({
+      url: "https://services1.arcgis.com/VuN78wcRdq1Oj69W/arcgis/rest/services/KKT_Community_Projects/FeatureServer",
+      featureReduction: clusterConfig,
+      outFields: ["*"],
+      popupTemplate: {
+        title: "{District}",
+        content: "This point represents a project in the {District} district."
       }
     });
-  });
+  
+    webmap.add(layer);
+  
+    var layerList = new LayerList({
+      view: view
+    });
+  
+    view.ui.add(layerList, "top-right");
+  
+    var attachments;
+  
+    view.when(function() {
+      attachments = new Attachments({
+        view: view,
+        layer: layer
+      });
+  
+      view.ui.add(attachments, "bottom-right");
+    });
+
+    view.on("click", function(event) {
+      view.hitTest(event).then(function(response) {
+        var result = response.results.filter(function(result) {
+          return result.graphic.layer === layer;
+        })[0];
+        if (result) {
+          layer.selectFeatures([result.graphic]);
+
+          // Set the feature for the Attachments widget
+          attachments.feature = result.graphic;
+        }
+      });
+    });
 });
